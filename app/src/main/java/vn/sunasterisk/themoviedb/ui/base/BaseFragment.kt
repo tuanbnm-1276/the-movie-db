c065bd045c19b3c2ac2c8b8ed8705fc95af646aa
tuanbnm-1276 <nguyen.minh.tuanb@sun-asterisk.com>
2019-11-24 23:12:22 +0700

    int base again

--- app/src/main/java/vn/sunasterisk/themoviedb/ui/base/BaseFragment.kt
+++ app/src/main/java/vn/sunasterisk/themoviedb/ui/base/BaseFragment.kt
@@ -1,4 +1,38 @@
 package vn.sunasterisk.themoviedb.ui.base
 
-class BaseFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+import vn.sunasterisk.themoviedb.BR
+
+abstract class BaseFragment<ViewBinding : ViewDataBinding, ViewModel : BaseViewModel> :
+    Fragment() {
+
+    lateinit var viewBinding: ViewBinding
+    abstract val viewModel: ViewModel
+    @get:LayoutRes
+    abstract val layoutId: Int
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        viewBinding = DataBindingUtil
+            .inflate(inflater, layoutId, container, false)
+        return viewBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        viewBinding.apply {
+            lifecycleOwner = viewLifecycleOwner
+            setVariable(BR.viewModel, viewModel)
+            executePendingBindings()
+        }
+    }
 }
